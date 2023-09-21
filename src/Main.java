429de6485cb5b6ae444d831f2a5bde77b0331393
BT_2N2_11 <sample@bths.edu>
2023-09-21 13:19:38 -0400

    Added final information of bill

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
         double costTotal;
 
         System.out.println("Welcome to the tip calculator!");
+        System.out.print("How many people are in your group: ");
+        int peopleAmount = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
         int tipPercent = scan.nextInt();
-        System.out.println();
 
         System.out.print("Enter a cost in dollars and cents (-1 to end): ");
         double cost = scan.nextDouble();
@@ -23,6 +24,16 @@ public class Main {
 
         }
 
-        System.out.println(costTotal);
+        double totalTip = costTotal * (tipPercent / 100.00);
+        double finalCost = costTotal - totalTip;
+
+        System.out.println("Total bill before tip: $" + costTotal);
+        System.out.println("Total percentage: " + tipPercent + "%");
+        System.out.println("Total tip: $" + totalTip);
+        System.out.println("Total bill with tip: $" + finalCost);
+        System.out.println("Per person cost before tip: $" + (costTotal / peopleAmount));
+        System.out.println("Tip per person: $" + (totalTip / peopleAmount));
+        System.out.println("Total cost per person: $" + (finalCost / peopleAmount));
+
     }
 }
