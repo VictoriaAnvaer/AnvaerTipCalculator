ff57f939963e5760c8c7fc7d80cab3045420b5a8
Victoria <victoriaa127@nycstudents.net>
2023-09-28 17:44:43 -0400

    fixed array and added the ability to calculate multiple times without rerunning code

--- src/Main.java
+++ src/Main.java
@@ -12,50 +12,75 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // variables declared
+
         double costTotal;
+        double totalTip;
+        double finalCost;
+        int peopleAmount;
+        int tipPercent;
+        double cost;
+
+        int x = 1;
         ArrayList<String> menuItem = new ArrayList<String>(); // creates array for storing menu items
 
         System.out.println("Welcome to the tip calculator!");
-        System.out.print("How many people are in your group: ");
-        int peopleAmount = scan.nextInt();
-        System.out.print("What's the tip percentage? (0-100): ");
-        int tipPercent = scan.nextInt();
 
-        System.out.print("Enter a cost in dollars and cents (-1 to end): ");
-        double cost = scan.nextDouble();
-        costTotal = cost + 1;
+        while (x != -1) { // while loop gives the ability to calculate multiple situations
+            System.out.print("How many people are in your group: ");
+            peopleAmount = scan.nextInt();
+            System.out.print("What's the tip percentage? (0-100): ");
+            tipPercent = scan.nextInt();
 
-        System.out.print("Enter a menu item: ");
-        menuItem.add(scan.next());
+            System.out.print("Enter a cost in dollars and cents (-1 to end): ");
+            cost = scan.nextDouble();
+            scan.nextLine();
+            costTotal = cost + 1; // +1 to not count -1 to quit
 
-
-        while (cost != -1) {
+            System.out.print("Enter a menu item: ");
+            menuItem.add(scan.nextLine());
 
             System.out.print("Enter a cost in dollars and cents (-1 to end): ");
             cost = scan.nextDouble();
             costTotal = costTotal + cost;
 
-            if (cost != -1) { // makes sure the code does not ask for an item if price is entered as -1 (to end)
+
+            while (cost != -1) { // while loop to give the ability to add multiple items
+
                 System.out.print("Enter a menu item: ");
                 menuItem.add(scan.next());
+                scan.nextLine();
+
+                System.out.print("Enter a cost in dollars and cents (-1 to end): ");
+                cost = scan.nextDouble();
+                costTotal = costTotal + cost;
+
             }
 
-        }
-        double totalTip = (Math.round((costTotal * (tipPercent / 100.00)) * 100.0) / 100.0);
-        double finalCost = costTotal + totalTip;
+            // main calculations for receipt in order to make it easier to read
 
-        System.out.println("Total bill before tip: $" + costTotal);
-        System.out.println("Total percentage: " + tipPercent + "%");
-        System.out.println("Total tip: $" + totalTip);
-        System.out.println("Total bill with tip: $" + finalCost);
-        System.out.println("Per person cost before tip: $" + (Math.round((costTotal / peopleAmount) * 100.0) / 100.0));
-        System.out.println("Tip per person: $" + (Math.round((totalTip / peopleAmount) * 100.0) / 100.0));
-        System.out.println("Total cost per person: $" + (Math.round((finalCost / peopleAmount) * 100.0) / 100.0));
+            totalTip = (Math.round((costTotal * (tipPercent / 100.00)) * 100.0) / 100.0);
+            finalCost = costTotal + totalTip;
+
+            // final receipt
+
+            System.out.println("Total bill before tip: $" + costTotal);
+            System.out.println("Total percentage: " + tipPercent + "%");
+            System.out.println("Total tip: $" + totalTip);
+            System.out.println("Total bill with tip: $" + finalCost);
+            System.out.println("Per person cost before tip: $" + (Math.round((costTotal / peopleAmount) * 100.0) / 100.0));
+            System.out.println("Tip per person: $" + (Math.round((totalTip / peopleAmount) * 100.0) / 100.0));
+            System.out.println("Total cost per person: $" + (Math.round((finalCost / peopleAmount) * 100.0) / 100.0));
 
 
-        System.out.println("Items ordered: "); // prints all menu items ordered from the array list
-        for(String i : menuItem){
-            System.out.println(i);
+            System.out.println("Items ordered: "); // prints all menu items ordered from the array list
+            for (String i: menuItem) {
+                System.out.println(i);
+            }
+
+            System.out.print("Enter any integer to perform another calculation, -1 to quit: ");
+            x = scan.nextInt();
+            System.out.println();
         }
     }
 }
